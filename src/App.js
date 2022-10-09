f326f3a9245f73cbf8800df5539324480ba625c7
E'laishah Ramirez <drz0224@gmail.com>
2022-10-09 12:59:36 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // Import data
-
+import { postings } from './postings.js'
 // Import components
 import './App.css';
+import SearchBar from './searchBar';
+import Directory from './directory';
+import Gallery from './gallery';
+import SideBar from './sidebar';
 
-function App(){
+function App() {
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        {     }
+          <SearchBar />
+          <Directory />
+          <Gallery postings={postings}/>
+          <SideBar />
       </div>
     </div>
   );
